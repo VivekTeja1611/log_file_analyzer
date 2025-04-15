6ed2cdfa919bcd5839d76d743bd1fcda89194db8
VivekTeja <Vivektejaoppo1611@gmail.com>
2025-04-15 09:41:52 +0000

    added templtes

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, redirect, render_template, request, send_file
-from werkzeug.utils import secure_filename
-import subprocess
 import os
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -14,9 +13,8 @@ def success():
     if request.method == 'POST':
         f = request.files['file']
         f.save('Apache_2k.log')
-
-        # run the script with os.system (not subprocess)
-        os.system('bash script.sh Apache_2k.log')       
+        os.system('bash script.sh Apache_2k.log')  
+        
         return render_template("acknowledgment.html")
-
+ 
 app.run(debug=True)
--- script.sh
+++ script.sh
@@ -14,15 +14,42 @@ fi
 awk 'BEGIN {
     FS = ",";
     OFS = ",";
-                 }
+    E1="jk2_init() Found child <*> in scoreboard slot <*>"
+    E2="workerEnv.init() ok <*>"
+    E3="mod_jk child workerEnv in error state <*>"
+    E4="[client <*>] Directory index forbidden by rule: <*>"
+    E5="jk2_init() Can't find child <*> in scoreboard"
+    E6="mod_jk child init <*> <*>"
+                                   }
 NR == 1 { $1="LineID,"  $1;
     print $0;
     next;
-}
+             }
 {
     $1 = (NR - 1) "," $1;
+    if [[ $4 =~ $E1 ]];then 
+        $5="E1"
+        $6=$E1
+    elif [[ $4 =~ $E2 ]];then
+      $5="E2"
+      $6=$E2
+    elif [[ $4 =~ $E3 ]];then
+      $5="E3"
+      $6=$E3  
+    elif [[ $4 =~ $E4 ]];then
+       $5="E4"
+       $6=$E4
+    elif [[ $4 =~ $E5 ]];then
+       $5="E5"
+       $6=$E5
+    elif [[ $4 =~ $E2 ]];then
+      $5="E6"
+      $6=$E6   
+    fi   
     print $0;
 }' static/Apache_2k.csv > Apache_2k_numbered.csv
+
+
 if [[ "$?" == "0" ]]; then
 echo "awk processs done too"
 fi
